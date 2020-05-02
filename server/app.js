5f5b9a6069a4c98dcd4302433138330275724eb4
syyad <syyadkhan@yahoo.com>
2020-05-02 16:53:49 -0400

    base mern app

--- server/app.js
+++ server/app.js
@@ -6,6 +6,16 @@ require("dotenv").config();
 
 const app = express();
 
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log("Connection Error: ", err));
+
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
