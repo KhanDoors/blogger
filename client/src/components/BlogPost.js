2c4de522e68ebd4bbeaff59d4e7cbbdaead6bcc6
Syyad Khan <syykhan@deloitte.com>
2020-05-04 19:14:16 -0400

    read slug done

--- client/src/components/BlogPost.js
+++ client/src/components/BlogPost.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const BlogPost = (props) => {
-  console.log(props);
-  return <div>{JSON.stringify(props)}</div>;
+  const [blogPost, setBlogPost] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_URL}/blogpost/${props.match.params.slug}`)
+      .then((res) => setBlogPost(res.data))
+      .catch((err) => {
+        console.log(err.response);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>{blogPost.title}</h1>
+      <h3>{blogPost.content}</h3>
+      <h3>{blogPost.user}</h3>
+      <h3>{new Date(blogPost.createdAt).toLocaleString()}</h3>
+    </div>
+  );
 };
 
 export default BlogPost;
