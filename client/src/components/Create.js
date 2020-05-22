aee0492be92b2c1e79dabee005c7958ea7e404fe
syyad <syyadkhan@yahoo.com>
2020-05-22 16:42:02 -0400

    import update

--- client/src/components/Create.js
+++ client/src/components/Create.js
@@ -1,19 +1,13 @@
 import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
-import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
 import Button from "@material-ui/core/Button";
 import axios from "axios";
-import Typography from "@material-ui/core/Typography";
 import CardMedia from "@material-ui/core/CardMedia";
 import CardHeader from "@material-ui/core/CardHeader";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
 import { getUser, getToken } from "./Utils";
 import CardContent from "@material-ui/core/CardContent";
-import Box from "@material-ui/core/Box";
 import CKEditor from "react-ckeditor-component";
 
 const useStyles = makeStyles((theme) => ({
--- client/src/components/UpdateBlogPost.js
+++ client/src/components/UpdateBlogPost.js
@@ -1,19 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
 import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import CardActionArea from "@material-ui/core/CardActionArea";
 import TextField from "@material-ui/core/TextField";
-import CardMedia from "@material-ui/core/CardMedia";
 import CardHeader from "@material-ui/core/CardHeader";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
-import Container from "@material-ui/core/Container";
+import CardMedia from "@material-ui/core/CardMedia";
 import { getToken } from "./Utils";
 import CKEditor from "react-ckeditor-component";
 
